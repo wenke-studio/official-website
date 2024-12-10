d12579ada48f5597f899b72a5c0e1bd1f1c05670
wenke.lai <wenke.studio@gmail.com>
2024-12-10 10:30:27 +0800

    feat: remove skeleton packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,7 @@
 import type { Config } from "tailwindcss";
 
-import { contentPath, skeleton } from "@skeletonlabs/skeleton/plugin";
-import * as themes from "@skeletonlabs/skeleton/themes";
-
 export default {
-  content: ["./src/**/*.{html,js,svelte,ts}", contentPath(import.meta.url, "svelte")],
+  content: ["./src/**/*.{html,js,svelte,ts}"],
 
   darkMode: "selector",
 
@@ -12,10 +9,5 @@ export default {
     extend: {},
   },
 
-  plugins: [
-    skeleton({
-      // NOTE: each theme included will be added to your CSS bundle
-      themes: [themes.rocket],
-    }),
-  ],
+  plugins: [],
 } satisfies Config;
